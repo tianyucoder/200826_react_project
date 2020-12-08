ce3647fc9c407fcec43d9bad3a46559e9562a328
tianyu5177 <zhangtianyu5177@sina.com>
2020-12-08 09:36:14 +0800

    更新

---
+++ 4_src_脚手架中的rem适配/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './utils/rem'
+import './App.less'
+
+export default class App extends Component {
+	render() {
+		return (
+			<div className="demo">
+				
+			</div>
+		)
+	}
+}
